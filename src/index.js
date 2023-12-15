4a279e02353763ad43dc457198c0eb1d061343a6
Joha <jotz@outlook.fr>
2023-12-15 16:14:47 +0100

    Added JS function using moment and moment timezone to show real time data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+function updateTime() {
+  // New York
+  let NYElement = document.querySelector("#newyork");
+  let NYDateElement = NYElement.querySelector(".date");
+  let NYTimeElement = NYElement.querySelector(".time");
+  let NYTime = moment().tz("America/New_York");
+  NYDateElement.innerHTML = NYTime.format("MMMM Do, YYYY");
+  NYTimeElement.innerHTML = NYTime.format("h:mm:ss [<small>]A[</small>]");
 
+  // Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do, YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
