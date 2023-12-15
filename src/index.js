80af32a8d5a6c4c69bb47eed8fd6f5b330f4b505
Joha <jotz@outlook.fr>
2023-12-15 17:11:52 +0100

    Added the current location to my project
    - added a new option in the html file for "My current timezone"
    - updated the code using moment.tz.guess() to enable current location

--- src/index.js
+++ src/index.js
@@ -24,6 +24,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimezone = event.target.value;
+  if (cityTimezone === "current") {
+    cityTimezone = moment.tz.guess();
+  }
   let cityName = cityTimezone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimezone);
   let citiesElement = document.querySelector("#cities");
